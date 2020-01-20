3d9adfa4cbaad5a42092b51bea4619978fbb733d
Sw24sX <aleksandr.korolyov.99@users.noreply.github.com>
2020-01-20 16:37:13 +0500

    Добавьте файлы проекта.

---
+++ DatePhoto.cs
@@ -0,0 +1,14 @@
+﻿using System.IO;
+using System.Globalization;
+using System.Windows.Media.Imaging;
+
+namespace SortingPhotos
+{
+    class DatePhoto
+    {
+        public void GetDatePhoto(string path)
+        {
+
+        }
+    }
+}
